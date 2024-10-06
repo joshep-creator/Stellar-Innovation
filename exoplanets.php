8d06afbb2a3379b5e65ab0951400f3a13996ad29
joshep-creator <joshepvega15@gmail.com>
2024-10-06 11:40:49 -0500

    10 planetas

--- exoplanets.php
+++ exoplanets.php
@@ -196,6 +196,251 @@
                 </div>
             </div>
 
+            <!-- single planet 1 -->
+            <div class="col-lg-3 col-md-4 col-sm-1 mb-4">
+                <?php $contador = 0; ?>
+                <div class="card">
+                    <img class="card-img-top img-fluid" src="src/img/planet_1.jpg" style="height: 150px; width: auto;">
+                    <div class="card-body">
+                        <h6>planet..nombrhfhfhfhfhfhfhe</h6>
+                        <div class="price">
+                            <!--Actual year-->
+                            <h6>Year Discover<strong>Year</strong></h6>
+                            <!--Before price-->
+                            <h6 class="l-through"></h6>
+                        </div>
+                        <button type="submit" class="btn btn-primary" id="verMasButton<?php echo $contador; ?>">Ver Más</button>
+                        <dialog id="miDialog<?php echo $contador; ?>">
+                            <h6>nasa_vch_descripction</h6>
+                            <h6>The distance to Earth</h6>nasa_dec_distance<h6>Persecs</h6>
+                            <button type="button" class="btn btn-secondary" id="cerrarButton<?php echo $contador; ?>">Cerrar</button>
+                        </dialog>
+                        <script>
+                            // Obtener referencias a los elementos del DOM utilizando el identificador único
+                            const verMasButton<?php echo $contador; ?> = document.getElementById('verMasButton<?php echo $contador; ?>');
+                            const miDialog<?php echo $contador; ?> = document.getElementById('miDialog<?php echo $contador; ?>');
+                            const cerrarButton<?php echo $contador; ?> = document.getElementById('cerrarButton<?php echo $contador; ?>');
+
+                            // Agregar un controlador de eventos al botón para abrir la ventana emergente
+                            verMasButton<?php echo $contador; ?>.addEventListener('click', () => {
+                                miDialog<?php echo $contador; ?>.showModal();
+                            });
+
+                            // Agregar un controlador de eventos al botón de cerrar la ventana emergente
+                            cerrarButton<?php echo $contador; ?>.addEventListener('click', () => {
+                                miDialog<?php echo $contador; ?>.close();
+
+                                <?php $contador++; ?>
+                            });
+                        </script>
+                    </div>
+                </div>
+            </div>
+
+            <!-- single planet 1 -->
+            <div class="col-lg-3 col-md-4 col-sm-1 mb-4">
+                <?php $contador = 0; ?>
+                <div class="card">
+                    <img class="card-img-top img-fluid" src="src/img/planet_1.jpg" style="height: 150px; width: auto;">
+                    <div class="card-body">
+                        <h6>planet..nombrhfhfhfhfhfhfhe</h6>
+                        <div class="price">
+                            <!--Actual year-->
+                            <h6>Year Discover<strong>Year</strong></h6>
+                            <!--Before price-->
+                            <h6 class="l-through"></h6>
+                        </div>
+                        <button type="submit" class="btn btn-primary" id="verMasButton<?php echo $contador; ?>">Ver Más</button>
+                        <dialog id="miDialog<?php echo $contador; ?>">
+                            <h6>nasa_vch_descripction</h6>
+                            <h6>The distance to Earth</h6>nasa_dec_distance<h6>Persecs</h6>
+                            <button type="button" class="btn btn-secondary" id="cerrarButton<?php echo $contador; ?>">Cerrar</button>
+                        </dialog>
+                        <script>
+                            // Obtener referencias a los elementos del DOM utilizando el identificador único
+                            const verMasButton<?php echo $contador; ?> = document.getElementById('verMasButton<?php echo $contador; ?>');
+                            const miDialog<?php echo $contador; ?> = document.getElementById('miDialog<?php echo $contador; ?>');
+                            const cerrarButton<?php echo $contador; ?> = document.getElementById('cerrarButton<?php echo $contador; ?>');
+
+                            // Agregar un controlador de eventos al botón para abrir la ventana emergente
+                            verMasButton<?php echo $contador; ?>.addEventListener('click', () => {
+                                miDialog<?php echo $contador; ?>.showModal();
+                            });
+
+                            // Agregar un controlador de eventos al botón de cerrar la ventana emergente
+                            cerrarButton<?php echo $contador; ?>.addEventListener('click', () => {
+                                miDialog<?php echo $contador; ?>.close();
+
+                                <?php $contador++; ?>
+                            });
+                        </script>
+                    </div>
+                </div>
+            </div>
+
+            <!-- single planet 1 -->
+            <div class="col-lg-3 col-md-4 col-sm-1 mb-4">
+                <?php $contador = 0; ?>
+                <div class="card">
+                    <img class="card-img-top img-fluid" src="src/img/planet_1.jpg" style="height: 150px; width: auto;">
+                    <div class="card-body">
+                        <h6>planet..nombrhfhfhfhfhfhfhe</h6>
+                        <div class="price">
+                            <!--Actual year-->
+                            <h6>Year Discover<strong>Year</strong></h6>
+                            <!--Before price-->
+                            <h6 class="l-through"></h6>
+                        </div>
+                        <button type="submit" class="btn btn-primary" id="verMasButton<?php echo $contador; ?>">Ver Más</button>
+                        <dialog id="miDialog<?php echo $contador; ?>">
+                            <h6>nasa_vch_descripction</h6>
+                            <h6>The distance to Earth</h6>nasa_dec_distance<h6>Persecs</h6>
+                            <button type="button" class="btn btn-secondary" id="cerrarButton<?php echo $contador; ?>">Cerrar</button>
+                        </dialog>
+                        <script>
+                            // Obtener referencias a los elementos del DOM utilizando el identificador único
+                            const verMasButton<?php echo $contador; ?> = document.getElementById('verMasButton<?php echo $contador; ?>');
+                            const miDialog<?php echo $contador; ?> = document.getElementById('miDialog<?php echo $contador; ?>');
+                            const cerrarButton<?php echo $contador; ?> = document.getElementById('cerrarButton<?php echo $contador; ?>');
+
+                            // Agregar un controlador de eventos al botón para abrir la ventana emergente
+                            verMasButton<?php echo $contador; ?>.addEventListener('click', () => {
+                                miDialog<?php echo $contador; ?>.showModal();
+                            });
+
+                            // Agregar un controlador de eventos al botón de cerrar la ventana emergente
+                            cerrarButton<?php echo $contador; ?>.addEventListener('click', () => {
+                                miDialog<?php echo $contador; ?>.close();
+
+                                <?php $contador++; ?>
+                            });
+                        </script>
+                    </div>
+                </div>
+            </div>
+
+            <!-- single planet 1 -->
+            <div class="col-lg-3 col-md-4 col-sm-1 mb-4">
+                <?php $contador = 0; ?>
+                <div class="card">
+                    <img class="card-img-top img-fluid" src="src/img/planet_1.jpg" style="height: 150px; width: auto;">
+                    <div class="card-body">
+                        <h6>planet..nombrhfhfhfhfhfhfhe</h6>
+                        <div class="price">
+                            <!--Actual year-->
+                            <h6>Year Discover<strong>Year</strong></h6>
+                            <!--Before price-->
+                            <h6 class="l-through"></h6>
+                        </div>
+                        <button type="submit" class="btn btn-primary" id="verMasButton<?php echo $contador; ?>">Ver Más</button>
+                        <dialog id="miDialog<?php echo $contador; ?>">
+                            <h6>nasa_vch_descripction</h6>
+                            <h6>The distance to Earth</h6>nasa_dec_distance<h6>Persecs</h6>
+                            <button type="button" class="btn btn-secondary" id="cerrarButton<?php echo $contador; ?>">Cerrar</button>
+                        </dialog>
+                        <script>
+                            // Obtener referencias a los elementos del DOM utilizando el identificador único
+                            const verMasButton<?php echo $contador; ?> = document.getElementById('verMasButton<?php echo $contador; ?>');
+                            const miDialog<?php echo $contador; ?> = document.getElementById('miDialog<?php echo $contador; ?>');
+                            const cerrarButton<?php echo $contador; ?> = document.getElementById('cerrarButton<?php echo $contador; ?>');
+
+                            // Agregar un controlador de eventos al botón para abrir la ventana emergente
+                            verMasButton<?php echo $contador; ?>.addEventListener('click', () => {
+                                miDialog<?php echo $contador; ?>.showModal();
+                            });
+
+                            // Agregar un controlador de eventos al botón de cerrar la ventana emergente
+                            cerrarButton<?php echo $contador; ?>.addEventListener('click', () => {
+                                miDialog<?php echo $contador; ?>.close();
+
+                                <?php $contador++; ?>
+                            });
+                        </script>
+                    </div>
+                </div>
+            </div>
+
+            <!-- single planet 1 -->
+            <div class="col-lg-3 col-md-4 col-sm-1 mb-4">
+                <?php $contador = 0; ?>
+                <div class="card">
+                    <img class="card-img-top img-fluid" src="src/img/planet_1.jpg" style="height: 150px; width: auto;">
+                    <div class="card-body">
+                        <h6>planet..nombrhfhfhfhfhfhfhe</h6>
+                        <div class="price">
+                            <!--Actual year-->
+                            <h6>Year Discover<strong>Year</strong></h6>
+                            <!--Before price-->
+                            <h6 class="l-through"></h6>
+                        </div>
+                        <button type="submit" class="btn btn-primary" id="verMasButton<?php echo $contador; ?>">Ver Más</button>
+                        <dialog id="miDialog<?php echo $contador; ?>">
+                            <h6>nasa_vch_descripction</h6>
+                            <h6>The distance to Earth</h6>nasa_dec_distance<h6>Persecs</h6>
+                            <button type="button" class="btn btn-secondary" id="cerrarButton<?php echo $contador; ?>">Cerrar</button>
+                        </dialog>
+                        <script>
+                            // Obtener referencias a los elementos del DOM utilizando el identificador único
+                            const verMasButton<?php echo $contador; ?> = document.getElementById('verMasButton<?php echo $contador; ?>');
+                            const miDialog<?php echo $contador; ?> = document.getElementById('miDialog<?php echo $contador; ?>');
+                            const cerrarButton<?php echo $contador; ?> = document.getElementById('cerrarButton<?php echo $contador; ?>');
+
+                            // Agregar un controlador de eventos al botón para abrir la ventana emergente
+                            verMasButton<?php echo $contador; ?>.addEventListener('click', () => {
+                                miDialog<?php echo $contador; ?>.showModal();
+                            });
+
+                            // Agregar un controlador de eventos al botón de cerrar la ventana emergente
+                            cerrarButton<?php echo $contador; ?>.addEventListener('click', () => {
+                                miDialog<?php echo $contador; ?>.close();
+
+                                <?php $contador++; ?>
+                            });
+                        </script>
+                    </div>
+                </div>
+            </div>
+
+            <!-- single planet 1 -->
+            <div class="col-lg-3 col-md-4 col-sm-1 mb-4">
+                <?php $contador = 0; ?>
+                <div class="card">
+                    <img class="card-img-top img-fluid" src="src/img/planet_1.jpg" style="height: 150px; width: auto;">
+                    <div class="card-body">
+                        <h6>planet..nombrhfhfhfhfhfhfhe</h6>
+                        <div class="price">
+                            <!--Actual year-->
+                            <h6>Year Discover<strong>Year</strong></h6>
+                            <!--Before price-->
+                            <h6 class="l-through"></h6>
+                        </div>
+                        <button type="submit" class="btn btn-primary" id="verMasButton<?php echo $contador; ?>">Ver Más</button>
+                        <dialog id="miDialog<?php echo $contador; ?>">
+                            <h6>nasa_vch_descripction</h6>
+                            <h6>The distance to Earth</h6>nasa_dec_distance<h6>Persecs</h6>
+                            <button type="button" class="btn btn-secondary" id="cerrarButton<?php echo $contador; ?>">Cerrar</button>
+                        </dialog>
+                        <script>
+                            // Obtener referencias a los elementos del DOM utilizando el identificador único
+                            const verMasButton<?php echo $contador; ?> = document.getElementById('verMasButton<?php echo $contador; ?>');
+                            const miDialog<?php echo $contador; ?> = document.getElementById('miDialog<?php echo $contador; ?>');
+                            const cerrarButton<?php echo $contador; ?> = document.getElementById('cerrarButton<?php echo $contador; ?>');
+
+                            // Agregar un controlador de eventos al botón para abrir la ventana emergente
+                            verMasButton<?php echo $contador; ?>.addEventListener('click', () => {
+                                miDialog<?php echo $contador; ?>.showModal();
+                            });
+
+                            // Agregar un controlador de eventos al botón de cerrar la ventana emergente
+                            cerrarButton<?php echo $contador; ?>.addEventListener('click', () => {
+                                miDialog<?php echo $contador; ?>.close();
+
+                                <?php $contador++; ?>
+                            });
+                        </script>
+                    </div>
+                </div>
+            </div>
 
         </div>
     </section>
